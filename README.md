f86ee1f295e64ac76352849e003210b1f1013880
MarkTension <tensen.mark@gmail.com>
2024-05-30 08:55:16 +0200

    new post

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 ## portfolio page
 
 TODO:
+- GA and react GA. Why is it not uploading?
 - add stuff to music bandcamp
 - add more posts
   - device 1
   - device 2
+- display recent article titles on the landing
+- maybe the spheres project i worked with at hypothetic
+- neural cellular automata for sound 
+- latest webgpu and midi project
+- some thoughts on my blog - markdown etc
--- src/components/landing.js
+++ src/components/landing.js
@@ -5,6 +5,7 @@ import ff1 from "../images/fireflies1.webp";
 import ff4 from "../images/fireflies2.webp";
 import { Item } from "./textConstants";
 import media from "../media";
+import posts from '../markdowns/index.json';
 
 const Hoverr = styled.h3`
     position: absolute;
@@ -20,25 +21,54 @@ const Hoverr = styled.h3`
         top: 10%;;
     `};
 `;
+class Landing extends React.Component {
+    constructor(props) {
+        super(props);
+        // add a state var that is called this.state.titles
+        this.state = { titles: [] }
+    }
 
-const Landing = props => {
-    return (
-        <div id="landing" style={{ marginTop: "0%", alignContent: "right", width: "100%", height: "100vh" }} >
-            <Hoverr> Mark Tensen</Hoverr>
+    // put all titles in the state
+    componentDidMount() {
+        this.state.titles = posts.files.map((post) => {
+            // return <Item key={post.title} style={{ width: "90%", textAlign: "right", fontSize: "0.9em" }}>{post.title}</Item>
+            return post.title
+        }
+        );
+        this.forceUpdate();
+    }
 
-            <div style={{ height: "80vh" }}>
-                <img src={ff0} alt="Logo" width={"23%"} />;
-                <img src={ff1} alt="Logo" width={"23%"} />;
-                <img src={ff4} alt="Logo" width={"23%"} />;
-            </div>
 
-            <Item style={{ width: "90%", textAlign: "right", fontSize: "0.9em", whiteSpace: "pre-line" }}>
-                On finding synergies between engineering, AI, music, artificial life, and generative design
-            </Item>
-            <Item style={{ width: "90%", textAlign: "right", fontSize: "0.9em", marginBottom: "10%" }}>
-                <a href="https://twitter.com/Mark_Tension">I share work in progress on twitter</a>, and music on <a href="https://open.spotify.com/artist/1lB15Q7MjR8s2j7TzeMP9Y?si=Z8aIfkyyT4qpf1HxKi2TdA&dl_branch=1">Spotify</a>.
-            </Item>
-        </div>
-    );
+    render(
+    ) {
+        return (
+            <div id="landing" style={{ marginTop: "0%", alignContent: "right", width: "100%", height: "100vh" }} >
+                <Hoverr> Mark Tensen</Hoverr>
+
+                <div style={{ height: "80vh" }}>
+                    <img src={ff0} alt="Logo" width={"23%"} />;
+                    <img src={ff1} alt="Logo" width={"23%"} />;
+                    <img src={ff4} alt="Logo" width={"23%"} />;
+                </div>
+                {/* <Item style={{ width: "90%", top: "45%", right: "20%", fontSize: "0.9em", position: "absolute" }}>
+
+                    {this.state.titles.map((title) => {
+                        console.log(title)
+                        return <Item>{title}</Item>
+                    })}
+
+                </Item > */}
+
+
+
+                <Item style={{ width: "90%", textAlign: "right", fontSize: "0.9em", whiteSpace: "pre-line" }}>
+                    on finding synergies between programming, music, AI, artificial life, and design
+                </Item>
+                <Item style={{ width: "90%", textAlign: "right", fontSize: "0.9em", marginBottom: "10%" }}>
+                    <a href="https://twitter.com/Mark_Tension">I share work in progress on twitter</a>, and music on <a href="https://open.spotify.com/artist/1lB15Q7MjR8s2j7TzeMP9Y?si=Z8aIfkyyT4qpf1HxKi2TdA&dl_branch=1">Spotify</a>.
+                </Item>
+            </div >
+        );
+    }
 };
 export default Landing
